1be851b14da47210cc7ab0861261aff7d258c770
Andrey <antonov.andrey.10101@gmail.com>
2024-03-08 18:50:57 +0300

    feat: add requests

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,16 @@
 function App() {
+  const fetchCatFact = async () => {
+    const response = await fetch('https://catfact.ninja/fact');
+    const data = await response.json();
+    return data.fact;
+  };
+
+  const fetchAge = async (name: string) => {
+    const response = await fetch(`https://api.agify.io/?name=${name}`);
+    const data = await response.json();
+    return data.age;
+  };
+
   return <div>Hello</div>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
-import './main.scss'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
