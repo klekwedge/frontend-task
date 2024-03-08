5439b340a0d4012f50c9343dd56813327e3654e4
Andrey <antonov.andrey.10101@gmail.com>
2024-03-08 18:59:24 +0300

    feat: add form and validation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,14 @@
 import { useState } from 'react';
+import * as yup from 'yup';
 import { useQuery } from 'react-query';
+import { useForm } from 'react-hook-form';
+
+const validationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(/^[a-zA-Z]+$/, 'Имя должно состоять только из букв')
+    .required('Введите имя'),
+});
 
 const fetchCatFact = async () => {
   const response = await fetch('https://catfact.ninja/fact');
@@ -14,6 +23,13 @@ const fetchAge = async (name: string) => {
 };
 
 function App() {
+  const {
+    handleSubmit: submitName,
+    control: nameControl,
+    errors: nameErrors,
+  } = useForm({
+    validationSchema,
+  });
   const { data: catFact, isLoading: catFactLoading, refetch: refetchCatFact } = useQuery('catFact', fetchCatFact);
   const [age, setAge] = useState<number | null>(null);
 
