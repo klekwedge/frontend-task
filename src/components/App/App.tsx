c0f0d27a6fd82c69e7ab32a568bb51792f224be7
Andrey <antonov.andrey.10101@gmail.com>
2024-03-13 16:13:27 +0300

    feat: change submit form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,18 @@ import { useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useMutation, useQuery } from 'react-query';
 import { Controller, useForm } from 'react-hook-form';
-import { AppRoot, Button, FormLayoutGroup, Input, Panel, PanelHeader, Spinner, View } from '@vkontakte/vkui';
+import {
+  AppRoot,
+  Button,
+  Div,
+  FormItem,
+  // FormLayoutGroup,
+  Input,
+  Panel,
+  PanelHeader,
+  Spinner,
+  View,
+} from '@vkontakte/vkui';
 // import bridge from '@vkontakte/vk-bridge';
 
 import validationSchema from '../../schemas';
@@ -17,14 +28,11 @@ import { TNameForm } from '../../types';
 
 function App() {
   const {
-    control,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<TNameForm>({
-    resolver: yupResolver(validationSchema),
-    mode: 'onSubmit',
-    criteriaMode: 'all',
-    reValidateMode: 'onChange',
+    resolver: yupResolver(validationSchema)
   });
 
   const [age, setAge] = useState<number | null>(null);
@@ -34,6 +42,7 @@ function App() {
     isLoading: catFactLoading,
     refetch: refetchCatFact,
   } = useQuery('catFact', CatService.getCatFact);
+
   const { mutate: fetchAgeMutation, isLoading: ageLoading } = useMutation(AgifyService.getAge, {
     onSuccess: (data) => {
       setAge(data);
@@ -49,29 +58,31 @@ function App() {
       <PanelHeader>Факты о котах</PanelHeader>
       <View activePanel="main" className="main">
         <Panel id="main">
-          <FormLayoutGroup onSubmit={onSubmitName}>
-            <Controller
-              name="name"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <Input {...field} type="text" placeholder="Введите имя" status={errors.name ? 'error' : undefined} />
-              )}
-            />
+          <form onSubmit={onSubmitName}>
+            <FormItem style={{ display: 'flex', gap: '20px' }}>
+              <Controller
+                name="name"
+                defaultValue=""
+                control={control}
+                render={({ field }) => (
+                  <Input {...field} type="text" placeholder="Введите имя" status={errors.name ? 'error' : 'default'} />
+                )}
+              />
+              <Button size="s" type="submit" disabled={ageLoading}>
+                Получить возраст
+              </Button>
+            </FormItem>
             {errors.name && <div>{errors.name.message}</div>}
-            <Button size="l" type="submit" disabled={ageLoading}>
-              Получить возраст
-            </Button>
             {ageLoading && <Spinner size="small" />}
-            {age !== null && <div>Ваш возраст: {age} лет</div>}
-          </FormLayoutGroup>
+            {!ageLoading && age !== null && <Div>Ваш возраст: {age} лет</Div>}
+          </form>
 
-          <div className="container">
+          <Div className="container">
             <Button onClick={() => refetchCatFact()} disabled={catFactLoading}>
               Загрузить факт о котах
             </Button>
-            {catFactLoading ? <Spinner size="small" /> : <div>{catFact}</div>}
-          </div>
+            {catFactLoading ? <Spinner size="small" /> : <Div>{catFact}</Div>}
+          </Div>
         </Panel>
       </View>
     </AppRoot>
